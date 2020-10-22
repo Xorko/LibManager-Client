6f098cff8a2715f4be2c359ef7a40f971445d82b
Thibault Le Goff <thib.legoff@gmail.com>
2020-10-22 15:58:47 +0200

    Add App.java
    Basic javafx application for testing gradle config. May be deleted later

---
+++ src/main/java/org/xorko/libmanager/App.java
@@ -0,0 +1,31 @@
+package org.xorko.libmanager;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        VBox content = new VBox();
+        Button btn = new Button("Hello world");
+        Scene scene = new Scene(root);
+
+        root.setCenter(content);
+        content.getChildren().add(btn);
+        content.setAlignment(Pos.CENTER);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("LibManager");
+        primaryStage.show();
+    }
+}
