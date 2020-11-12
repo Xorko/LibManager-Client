d38c0fcb19786ea73742321c3dda28f2263453f7
Thibault Le Goff <thib.legoff@gmail.com>
2020-11-12 18:22:02 +0100

    Add views and basic implementation

--- src/main/java/org/libmanager/client/App.java
+++ src/main/java/org/libmanager/client/App.java
@@ -1,14 +1,21 @@
 package org.libmanager.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class App extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootView;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,16 +23,39 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        BorderPane root = new BorderPane();
-        VBox content = new VBox();
-        Button btn = new Button("Hello world");
-        Scene scene = new Scene(root);
-
-        root.setCenter(content);
-        content.getChildren().add(btn);
-        content.setAlignment(Pos.CENTER);
-        primaryStage.setScene(scene);
+        this.primaryStage = primaryStage;
         primaryStage.setTitle("LibManager");
+        initRootView();
+        showReservationView();
         primaryStage.show();
+    }
+
+    public void initRootView() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("/view/RootView.fxml"));
+            rootView = loader.load();
+
+            Scene scene = new Scene(rootView);
+            primaryStage.setScene(scene);
+
+            //TODO Controller
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showReservationView() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("/view/ReservationView.fxml"));
+            GridPane reservation = loader.load();
+
+            rootView.setCenter(reservation);
+
+            //TODO Controller
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
