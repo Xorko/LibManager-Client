b778d79a3d754e17853a3d444a26e8811d508a86
BzhNouille <agoguebenoit@gmail.com>
2020-11-25 19:42:50 +0100

    Change fxml tags to match new views

--- src/main/java/org/libmanager/client/controller/EditItemController.java
+++ src/main/java/org/libmanager/client/controller/EditItemController.java
@@ -1,6 +1,7 @@
 package org.libmanager.client.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import org.libmanager.client.App;
@@ -8,16 +9,23 @@ import org.libmanager.client.App;
 
 public class EditItemController {
 
+    //TODO hide fields depending on item add
+
     @FXML
     private TextField titleField;
     @FXML
     private TextField authorField;
     @FXML
     private TextField genreField;
+    /*
+    TODO Type
     @FXML
-    private TextField typeField;
+    private ComboBox typeCBox;
+    */
     @FXML
     private DatePicker releaseDateDPicker;
+    @FXML
+    private TextField publisherField;
     @FXML
     private TextField isbnField;
     @FXML
